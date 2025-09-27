6ddae3c64a843187570816a73ef15f28e46edc37
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:49:20 -0400

    added css for MultipleCHoice.

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,10 +1,11 @@
+import "../styles/MultipleChoice.css";
 function MultipleChoice() {
   return (
     <div>
       <input type="radio" id="A" name="Choice" value="A"></input>
       <label htmlFor="A">A</label>
       <input type="radio" id="B" name="Choice" value="B"></input>
-      <label htmlFor="B">BBBBB</label>
+      <label htmlFor="B">B</label>
       <input type="radio" id="C" name="Choice" value="C"></input>
       <label htmlFor="C">C</label>
       <input type="radio" id="D" name="Choice" value="D"></input>
