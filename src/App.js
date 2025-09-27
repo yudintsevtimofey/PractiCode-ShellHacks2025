342c5e995f237057c2f89c4eb52e4bc412962fd0
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:53:08 -0400

    changed App to load Lobbies again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
-import LifeBar from "./components/LifeBar";
-import MainMenu from "./components/MainMenu";
 import Lobbies from "./components/Lobbies";
-import PlayerProgress from "./components/PlayerProgress";
-import QuestionBox from "./components/QuestionBox";
-import AnswerBox from "./components/AnswerBox";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AnswerBox></AnswerBox>
+        <Lobbies></Lobbies>
       </header>
     </div>
   );
