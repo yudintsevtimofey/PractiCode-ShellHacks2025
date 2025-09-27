701a0455bef036005319f2b3a8fbb16bf535e873
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 11:50:09 -0400

    nested our life bars within elements.

--- src/components/PlayerProgress.js
+++ src/components/PlayerProgress.js
@@ -3,12 +3,16 @@ import LifeBar from "./LifeBar";
 
 function PlayerProgress() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <header className="App-header">
+      <div className="container">
+        <p>Player 1</p>
         <LifeBar stops={1}></LifeBar>
+      </div>
+      <div className="container">
+        <p>Player 2</p>
         <LifeBar stops={5}></LifeBar>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
 
