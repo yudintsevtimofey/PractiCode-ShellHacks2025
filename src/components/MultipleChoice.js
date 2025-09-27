21644a53e8d05a4f5712707f2f60701e1461b262
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:21:46 -0400

    Removed Props MultipleChoice

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,34 +1,14 @@
-function MultipleChoice(props) {
+function MultipleChoice() {
   return (
     <div>
-      <input
-        type="radio"
-        id={props.ChoiceA}
-        name="fav_language"
-        value="HTML"
-      ></input>
-      <label for={props.ChoiceA}>{props.ChoiceA}</label>
-      <input
-        type="radio"
-        id={props.ChoiceB}
-        name="fav_language"
-        value="HTML"
-      ></input>
-      <label for={props.ChoiceB}>{props.ChoiceB}</label>
-      <input
-        type="radio"
-        id={props.ChoiceC}
-        name="fav_language"
-        value="HTML"
-      ></input>
-      <label for={props.ChoiceC}>{props.ChoiceC}</label>
-      <input
-        type="radio"
-        id={props.ChoiceD}
-        name="fav_language"
-        value="HTML"
-      ></input>
-      <label for={props.ChoiceD}>{props.ChoiceD}</label>
+      <input type="radio" id="A" name="fav_language" value="A"></input>
+      <label for="A">A</label>
+      <input type="radio" id="B" name="fav_language" value="B"></input>
+      <label for="B">B</label>
+      <input type="radio" id="C" name="fav_language" value="C"></input>
+      <label for="C">C</label>
+      <input type="radio" id="D" name="fav_language" value="D"></input>
+      <label for="D">D</label>
       <button></button>
     </div>
   );
