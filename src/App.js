78a31834505c2a9ddf4100e18395d5b34b839427
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 07:13:17 -0400

    App.js now has a div with Type Racer on it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>Type racer</div>
         <BackDrop></BackDrop>
       </header>
     </div>
