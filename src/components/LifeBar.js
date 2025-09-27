c598b7f99ed854807cbb4ae2d4c47eb2a0981970
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 11:14:22 -0400

    added non breaking spaces instead of nums

--- src/components/LifeBar.js
+++ src/components/LifeBar.js
@@ -5,176 +5,176 @@ function LifeBar(props) {
     case 0:
       return (
         <div className="container">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 1:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 2:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div>0</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 3:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 4:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 5:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
         </div>
       );
       break;
     case 6:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div className="tenPercentClear"> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
         </div>
       );
       break;
     case 7:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
+          <div> &nbsp;</div>
         </div>
       );
       break;
     case 8:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
-          <div></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 9:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div>&nbsp;</div>
         </div>
       );
       break;
     case 10:
       return (
         <div className="container">
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
-          <div className="tenPercentClear"></div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
+          <div className="tenPercentClear">&nbsp;</div>
         </div>
       );
       break;
