12f881cda95a47b2489b566eddf7f9790b1c4821
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:07:20 -0400

    added radio buttons

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,11 +1,13 @@
 function MultipleChoice(props) {
-  return (
-    <ul>
-      <li>
-        <Selection></Selection>
-      </li>
-    </ul>
-  );
+  return <div>
+    <input type="radio" id={props.ChoiceA} name="fav_language" value="HTML"></input>
+  <label for={props.ChoiceA}>{{props.ChoiceA}}</label>
+      <input type="radio" id={props.ChoiceB} name="fav_language" value="HTML"></input>
+  <label for={props.ChoiceB}>{{props.ChoiceB}}</label>
+      <input type="radio" id={props.ChoiceC} name="fav_language" value="HTML"></input>
+  <label for={props.ChoiceC}>{{props.ChoiceC}}</label>
+      <input type="radio" id={props.ChoiceD} name="fav_language" value="HTML"></input>
+  <label for={props.ChoiceD}>{{props.ChoiceD}}</label>
+  </div>;
 }
-
 export default MultipleChoice;
