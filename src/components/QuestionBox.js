4ca9b732e43f424d423fe6bc4c05a767d6cdf08e
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 12:44:58 -0400

    created QuestionBox component

---
+++ src/components/QuestionBox.js
@@ -0,0 +1,11 @@
+import "../styles/QuestionBox.css";
+
+function QuestionBox() {
+  return (
+    <div className="Box-Container">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default QuestionBox;
