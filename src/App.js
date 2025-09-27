1232805fd15e15af6d0c582a6addd77e14b7bc67
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 09:57:00 -0400

    App now sets LifeBar stops

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LifeBar></LifeBar>
+        <LifeBar stops={1}></LifeBar>
       </header>
     </div>
   );
