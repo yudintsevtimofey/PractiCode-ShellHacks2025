3e2419bcfdec788b1f7e50fc425a502b11d60820
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:26:40 -0400

    removed fav_language to the choices

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,13 +1,13 @@
 function MultipleChoice() {
   return (
     <div>
-      <input type="radio" id="A" name="fav_language" value="A"></input>
+      <input type="radio" id="A" value="A"></input>
       <label for="A">A</label>
-      <input type="radio" id="B" name="fav_language" value="B"></input>
+      <input type="radio" id="B" value="B"></input>
       <label for="B">B</label>
-      <input type="radio" id="C" name="fav_language" value="C"></input>
+      <input type="radio" id="C" value="C"></input>
       <label for="C">C</label>
-      <input type="radio" id="D" name="fav_language" value="D"></input>
+      <input type="radio" id="D" value="D"></input>
       <label for="D">D</label>
       <button>Submit</button>
     </div>
