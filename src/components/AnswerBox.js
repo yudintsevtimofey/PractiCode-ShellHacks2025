8d7b1b3acb2a4d3bfb6b961cac8a53629bd6ef50
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 13:25:38 -0400

    removed the prompt line

--- src/components/AnswerBox.js
+++ src/components/AnswerBox.js
@@ -4,8 +4,7 @@ function AnswerBox(props) {
   return (
     <div className="Box-Container">
       <header className="Box-header">
-        <p className="Answer-text">Question</p>
-        <p className="Answer-prompt">{props.Prompt}</p>
+        <p className="Answer-text">Answer</p>
       </header>
     </div>
   );
