08003e6c5798526028f799c78017b3a6b38cd475
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:10:41 -0400

    removed extra brackets.

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,13 +1,36 @@
 function MultipleChoice(props) {
-  return <div>
-    <input type="radio" id={props.ChoiceA} name="fav_language" value="HTML"></input>
-  <label for={props.ChoiceA}>{{props.ChoiceA}}</label>
-      <input type="radio" id={props.ChoiceB} name="fav_language" value="HTML"></input>
-  <label for={props.ChoiceB}>{{props.ChoiceB}}</label>
-      <input type="radio" id={props.ChoiceC} name="fav_language" value="HTML"></input>
-  <label for={props.ChoiceC}>{{props.ChoiceC}}</label>
-      <input type="radio" id={props.ChoiceD} name="fav_language" value="HTML"></input>
-  <label for={props.ChoiceD}>{{props.ChoiceD}}</label>
-  </div>;
+  return (
+    <div>
+      <input
+        type="radio"
+        id={props.ChoiceA}
+        name="fav_language"
+        value="HTML"
+      ></input>
+      <label for={props.ChoiceA}>{props.ChoiceA}</label>
+      <input
+        type="radio"
+        id={props.ChoiceB}
+        name="fav_language"
+        value="HTML"
+      ></input>
+      <label for={props.ChoiceB}>{props.ChoiceB}</label>
+      <input
+        type="radio"
+        id={props.ChoiceC}
+        name="fav_language"
+        value="HTML"
+      ></input>
+      <label for={props.ChoiceC}>{props.ChoiceC}</label>
+      <input
+        type="radio"
+        id={props.ChoiceD}
+        name="fav_language"
+        value="HTML"
+      ></input>
+      <label for={props.ChoiceD}>{props.ChoiceD}</label>
+      <button></button>
+    </div>
+  );
 }
 export default MultipleChoice;
