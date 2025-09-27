b8bd50c9bb2871e8ead5b8de6260604a583a3876
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 09:34:42 -0400

    added a list of divs with class tenPercentCLear

--- src/components/LifeBar.js
+++ src/components/LifeBar.js
@@ -2,7 +2,14 @@ import "../styles/LifeBar.css";
 function LifeBar() {
   return (
     <div className="container">
-      <p>bar</p>
+      <div className="tenPercentClear">1</div>
+      <div className="tenPercentClear">2</div>
+      <div className="tenPercentClear">3</div>
+      <div className="tenPercentClear">4</div>
+      <div className="tenPercentClear">5</div>
+      <div className="tenPercentClear">6</div>
+      <div className="tenPercentClear">7</div>
+      <div className="tenPercentClear">8</div>
     </div>
   );
 }
