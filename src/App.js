fad0d12f916d95f5d127b37a0aa8e10ef9e2568f
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 08:52:57 -0400

    Removed Backdrop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import BackDrop from "./components/Backdrop.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>Type racer</div>
-        <BackDrop></BackDrop>
         <div>WPM</div>
       </header>
     </div>
