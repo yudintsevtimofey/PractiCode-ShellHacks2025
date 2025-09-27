e81a0272b6ec52ece3c5fe8eac2c0e0da1bbaff1
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-09-27 15:47:16 -0400

    added htmlFor, names to fix labels

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,14 +1,14 @@
 function MultipleChoice() {
   return (
     <div>
-      <input type="radio" id="A" value="A"></input>
-      <label for="A">A</label>
-      <input type="radio" id="B" value="B"></input>
-      <label for="B">B</label>
-      <input type="radio" id="C" value="C"></input>
-      <label for="C">C</label>
-      <input type="radio" id="D" value="D"></input>
-      <label for="D">D</label>
+      <input type="radio" id="A" name="Choice" value="A"></input>
+      <label htmlFor="A">A</label>
+      <input type="radio" id="B" name="Choice" value="B"></input>
+      <label htmlFor="B">BBBBB</label>
+      <input type="radio" id="C" name="Choice" value="C"></input>
+      <label htmlFor="C">C</label>
+      <input type="radio" id="D" name="Choice" value="D"></input>
+      <label htmlFor="D">D</label>
       <button>Submit</button>
     </div>
   );
